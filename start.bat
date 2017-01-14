e94ab9a3032096b6dd312cd09a36946cd69b8f76
Alex Florin <alex.florin2352@gmail.com>
2017-01-14 19:07:45 +0200

    added a new process to the topology

--- start.bat
+++ start.bat
@@ -1,4 +1,5 @@
-start cmd /c "go run app.go" 8081 8081 8082 8083
-start cmd /c "go run app.go" 8082 8081 8082 8083
-start cmd /c "go run app.go" 8083 8081 8082 8083
+start cmd /c "go run app.go" 8081 8081 8082 8083 8084
+start cmd /c "go run app.go" 8082 8081 8082 8083 8084
+start cmd /c "go run app.go" 8083 8081 8082 8083 8084
+start cmd /c "go run app.go" 8084 8081 8082 8083 8084
 pause
